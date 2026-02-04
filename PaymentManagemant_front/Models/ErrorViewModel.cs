c15b741b2d437015b06cc29d0a1549573d1d6542
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-02-04 21:26:38 +0100

    Add project files.

---
+++ PaymentManagemant_front/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaymentManagemant_front.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
